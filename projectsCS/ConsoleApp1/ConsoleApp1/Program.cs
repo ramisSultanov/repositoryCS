de8feb0579ec797852da757af9006c7346ed6653
Ramis <rssultanrs@gmail.com>
2021-08-26 18:04:35 -0400

    trying to push the code 1

--- projectsCS/ConsoleApp1/ConsoleApp1/Program.cs
+++ projectsCS/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Code_Coach_Challenge
 {
     class Program
     {
+        static string BreakC(string str)
+        {
+            char[] charArray = str.ToCharArray();
+
+            List<string> list = new List<string>();
+            list.Add(Convert.ToString(charArray[0]));
+
+            for (int i = 1; i < charArray.Length; i++)
+            {
+                if (Convert.ToString(charArray[i]) == Convert.ToString(charArray[i]).ToUpper())
+                {
+                    list.Add(" ");
+                }
+                list.Add(Convert.ToString(charArray[i]));
+            }
+            str = string.Join("", list.ToArray());
+            return str;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(BreakC("camelCasing"));
+
         }
     }
+
 }
